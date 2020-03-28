c5597bc1a59d3df828c525667a51b03f41162aa9
Padma Naresh <padmanaresh1986@gmail.com>
2020-03-28 14:38:43 +0530

    VimCrypt~03!«*zô©{*côOEïÂ)RÂk6;¸,3¶æýxNí¨ÔÐùĚm'04RRU^Éu1¼È}%q¡°0(¢º¹ٱ౏HE7Ï'öõP­`øêÇáè_-Wfü_]MM«+F￐-º16QøÆãp^ÞÐð^;!¼<Æá}1¼KÒIfµªÄ]_÷yÜÔLòéj¤?äìÍö?½Îã+½æõÜpfómV©Lk9¾ì>]Põ6_ÈlËà3Èmb7m·i86P+SRFÓ>j"w;hU´Ã^{ÆS㤁ù ݀ÄRaS5;ÊàèêÕ]`ÿl)!½hâ)C"ÔZ¡¥no	Õ|âÎJ°L	ÞQ¾Q¦\ (ª¦n8D­gÙ»ü%	gl|±ËQ¾!ð¢XáÓ]3>*öÞ)

---
+++ threads/src/main/java/org/padmanaresh/Main.java
@@ -0,0 +1,14 @@
+package org.padmanaresh;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = """
+        This is simple hello world program to say
+        hello world 
+        from 
+        java text block
+        """;
+        System.out.println(a);
+    }
+
+}
