0853698467f3ecba03f19d2893216b8be6709733
Padma Naresh <padmanaresh1986@gmail.com>
2020-03-29 10:45:49 +0530

    added complerable feature basic example

---
+++ threads/src/main/java/org/padmanaresh/completablefeature/examples/CompletableFutureExceptionExample.java
@@ -0,0 +1,60 @@
+package org.padmanaresh.completablefeature.examples;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+public class CompletableFutureExceptionExample {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("main thread started ...");
+        System.out.println("spawn new thread for network process  ...");
+        /**
+         * use supplyAsync when thread returns some value , else runAsync
+         */
+
+        CompletableFuture.supplyAsync(() -> longNetworkProcess(2))
+                .exceptionally(getThrowableIntegerFunction())
+                .thenApply(val -> processValue(val))
+                .exceptionally(getThrowableIntegerFunction())
+                .thenApply(val -> processValue(val))
+                .exceptionally(getThrowableIntegerFunction())
+                .thenAccept( result -> System.out.println( "Network Process replied with val " + result));
+                //.join();
+        // int j = longNetworkProcess(2);
+        //System.out.println(j);
+        System.out.println("meanwhile main thread doing some other job");
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("done ....");
+    }
+
+    private static Function<Throwable, Integer> getThrowableIntegerFunction() {
+        return throwable -> {
+            throwable.printStackTrace();
+            return null;
+        };
+    }
+
+    private static int longNetworkProcess(int i){
+        System.out.println("network process happening in separate thread "+ Thread.currentThread().getName());
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.getCause();
+        }
+        return i * i ;
+    }
+
+    private static int processValue(int i){
+        System.out.println("process value in thread " + Thread.currentThread().getName());
+        if(i%2 == 0){
+            i = i+1;
+        }else{
+            throw new RuntimeException(" value cannot be odd number ..");
+        }
+        return i;
+    }
+
+
+
+}
