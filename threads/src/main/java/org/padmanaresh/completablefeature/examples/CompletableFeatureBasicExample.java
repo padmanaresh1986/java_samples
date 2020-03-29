46d1dc86618743ac6fb6cc32689edddbbf4a194f
Padma Naresh <padmanaresh1986@gmail.com>
2020-03-29 09:13:43 +0530

    added complerable feature basic example

---
+++ threads/src/main/java/org/padmanaresh/completablefeature/examples/CompletableFeatureBasicExample.java
@@ -0,0 +1,35 @@
+package org.padmanaresh.completablefeature.examples;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFeatureBasicExample {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("main thread started ...");
+        System.out.println("spawn new thread for network process  ...");
+        /**
+         * use supplyAsync when thread returns some value , else runAsync
+         */
+
+        CompletableFuture.supplyAsync(() -> longNetworkProcess(2))
+                .thenAccept( result -> System.out.println( "Network Process replied with val " + result));
+        // int j = longNetworkProcess(2);
+        //System.out.println(j);
+        System.out.println("meanwhile main thread doing some other job");
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("done ....");
+    }
+    private static int longNetworkProcess(int i){
+        System.out.println("network process happening in separate thread "+ Thread.currentThread().getName());
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return i * i ;
+    }
+
+
+
+}
